74ced79232d0418975dc90d6f1744d81bf97a263
James Thomas <F0llydog@protonmail.com>
2017-07-03 21:06:50 +0100

    now one js function

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
+import random
 from flask import Flask, render_template, json
 
 
 app = Flask(__name__)
 
+words = ["boxer", "dickhead", "foolish"]
 
 @app.route('/')
 def index():
-    anagram = ["oxber", "boxer"]
-    return render_template("index.html", anagram=anagram)
+    word = random.choice(words)
+    anagram = list(word)
+    random.shuffle(anagram)
+    data = [''.join(anagram), word]
+    return render_template("index.html", data=data)
