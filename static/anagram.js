076deff2baa20d0a7e921e65d3af7c69337a9e42
James Thomas <F0llydog@protonmail.com>
2017-07-03 23:05:10 +0100

    callback AJAX ALMOST done?

--- static/anagram.js
+++ static/anagram.js
@@ -1,3 +1,30 @@
+function getNewAnagram (url, methodType, callback) {
+    xhr = new XMLHttpRequest();
+    xhr.open(methodType, url);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readystate === 4) {
+            if (xhr.status === 200) {
+                const resp = xhr.responsetext;
+                const respJson = JSON.parse(resp);
+                let [anagram, solution] = respJson;
+                callback(anagram);
+            } else {
+                console.log("xhr failed");
+            }
+        } else {
+            console.log("xhr being processed");
+        }
+    }
+}
+
+
+function displayAnagram (data) {
+  const target = document.getElementById("word-place");
+  target.innerText = data;
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const [anagram, solution] = wordData;
   const target = document.getElementById("word-place");
