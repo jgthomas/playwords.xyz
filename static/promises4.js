22b0f1f31b83d93ee28e9b361b12463272a3e716
James Thomas <F0llydog@protonmail.com>
2017-07-05 19:09:13 +0100

    basic promise-ajax-wordload

--- static/promises4.js
+++ static/promises4.js
@@ -17,31 +17,39 @@ function get(url) {
 }
 
 
-get('127.0.0.1:5000/anagram').then( (result) => {
+/*get('127.0.0.1:5000/anagram').then( (result) => {
     const data = JSON.parse(result);
     return data;
 }).catch( (error) => {
     return ["Error: ", error];
-})
+})*/
 
 
 document.addEventListener("DOMContentLoaded", function () {
     const playButton = document.getElementById("play");
-    playButton.addEventListener("click", function () {
-        const [anagram, solution] = getAnagram2();
+    playButton.addEventListener("click", get('http://127.0.0.1:5000/anagram').then( (result) => {
+        console.log("executing");
+        const [anagram, solution] = JSON.parse(result);
+        console.log(anagram, solution);
         const target = document.getElementById("word-place");
         target.innerText = anagram;
         const guessForm = document.getElementById("guess-form");
-        guessForm.addEventListener("submit", function (e) {
+        guessForm.addEventListener("submit", (e) => {
+            console.log("executing 2");
             const anagramGuess = document.getElementById("guess").value;
-            const answer = document.getElementById("result");
+            console.log(anagramGuess);
+            const result = document.getElementById("result");
             if (anagramGuess === solution) {
-                answer.innerText = "CORRECT!";
+                result.innerText = "CORRECT!";
             } else {
-                answer.innerText = "Try Again...";
+                result.innerText = "Try Again...";
             } 
             e.preventDefault();
+            /*e.stopPropagation;*/
             guessForm.reset();
         });
-    });
+        })
+        .catch( (error) => {
+            target.innerText = error;
+        }));
 });
