c4888c4f4896aa5827267e805143ea9cbc34d55f
James Thomas <dairy_water@yahoo.co.uk>
2017-07-09 09:43:08 +0100

    word functions in own module

--- application.py
+++ application.py
@@ -1,32 +1,12 @@
 import random
 from flask import Flask, render_template, jsonify
-
+from words import feed_filter, load_words, data_filter, is_length
 
 app = Flask(__name__)
 
 
-word_file = "50k.txt"
-
-
-def feed_filter(feed, criterion=None, *comparison):
-    for element in feed:
-        element = element.strip()
-        if criterion:
-            if not criterion(element, *comparison):
-                continue
-        if not element:
-            continue
-        yield element
-
-
-def load_words(word_file, criterion=None, *comparison):
-    with open(word_file) as filename:
-        return [word for word
-                in feed_filter(filename, criterion, *comparison)]
-
-
-def is_length(item, length):
-    return len(item) == length
+WORD_FILE = "50k.txt"
+FULL_WORD_LIST = load_words(WORD_FILE)
 
 
 @app.route('/')
@@ -36,8 +16,8 @@ def index():
 
 @app.route('/anagram', methods=['GET'])
 def anagram():
-    words = load_words(word_file)
-    word = random.choice(words)
+    #words = data_filter(word_file)
+    word = random.choice(FULL_WORD_LIST)
     anagram = list(word)
     random.shuffle(anagram)
     data = [''.join(anagram), word]
