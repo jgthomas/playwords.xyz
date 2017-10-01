fd7d6695721cb95d18dcc44ea0abdad36d61d9ba
James Thomas <codeactor@protonmail.com>
2017-10-01 08:59:04 +0100

    pre-written queries for adding player and games

---
+++ queries.py
@@ -0,0 +1,69 @@
+# PLAYER queries
+
+CREATE_PERSON = """CREATE TABLE IF NOT EXISTS person (
+                       player_id  INTEGER PRIMARY KEY NOT NULL, 
+                       username   TEXT UNIQUE NOT NULL, 
+                       password   TEXT NOT NULL, 
+                       join_date  DATE NOT NULL)"""
+
+
+GET_ALL_PERSON = """SELECT *
+                      FROM person"""
+
+
+
+SELECT_PERSON = """SELECT *
+                     FROM person
+                    WHERE username=?"""
+
+
+ADD_PERSON = """INSERT INTO person (username, password, join_date)
+                VALUES (?, ?, ?)"""
+
+
+
+# GAME queries
+
+CREATE_GAME = """CREATE TABLE IF NOT EXISTS game (
+                       game_id     INTEGER PRIMARY KEY,
+                       game_type   INTEGER NOT NULL,
+                       score       INTEGER NOT NULL,
+                       [timestamp] TIMESTAMP NOT NULL,
+                       player_id   INTEGER NOT NULL,
+                       FOREIGN KEY(player_id) REFERENCES person(player_id))"""
+
+
+ADD_GAME = """INSERT INTO game (game_type, score, timestamp, player_id)
+              VALUES (?, ?, ?, ?)"""
+
+
+GET_PLAYER_GAMES = """SELECT game_id, game_type, score
+                        FROM game
+                  INNER JOIN person
+                          ON game.player_id = person.player_id
+                       WHERE person.player_id=?"""
+
+
+BEST_SCORE = """SELECT MAX(score) AS best_score
+                  FROM game
+            INNER JOIN person
+                    ON game.player_id = person.player_id
+                 WHERE game_type=?
+                   AND person.player_id=?"""
+
+
+AVERAGE_SCORE = """SELECT AVG(score) AS average_score
+                     FROM game
+               INNER JOIN person
+                       ON game.player_id = person.player_id
+                    WHERE game_type=?
+                      AND person.player_id=?"""
+
+
+ALL_SCORES = """SELECT score
+                  FROM game
+            INNER JOIN person
+                    ON game.player_id = person.player_id
+                 WHERE game_type=? 
+                   AND person.player_id=?
+                 ORDER BY score DESC"""
